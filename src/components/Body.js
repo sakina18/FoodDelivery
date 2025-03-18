aa27bc86d3ffff60d9acfd41fbbf59a924d23e0c
sakina18 <sakinabintebashir3@gmail.com>
2025-03-18 15:57:47 +0530

    first commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,9 @@ const Body = () => {
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button className="filter-btn" onClick={()=> {
+          console.log("button clicked");}
+          }>Top Rated Restaurants</button>
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant) => (
