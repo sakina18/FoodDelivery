1a0afd681d9273cf58c024b5120bcfb30876e0c3
sakina18 <sakinabintebashir3@gmail.com>
2025-03-18 16:55:58 +0530

    first commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,33 +3,8 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(  [
-   {info:{
-    id: "43836", 
-    name: "McDonald's",
-    cloudinaryImageId:
-      "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/5e7991d9-6a0d-4034-82a9-59bd2bbdc0b3_43836.jpg",
-    locality: "MG Road",
-    areaName: "Ashok Nagar",
-    costForTwo: "₹400 for two",
-    cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
-    avgRating: 4.4,
-    parentId: "630",
-    avgRatingString: "4.4",
-    totalRatingsString: "25K+"}},
-    {info: { id: "43846",
-      name: "albaik",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/5e7991d9-6a0d-4034-82a9-59bd2bbdc0b3_43836.jpg",
-      locality: "MG Road",
-      areaName: "Ashok Nagar",
-      costForTwo: "₹400 for two",
-      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
-      avgRating: 3.4,
-      parentId: "630",
-      avgRatingString: "3.4",
-      totalRatingsString: "25K+"}}]
-  );
+  const [listOfRestaurants, setListOfRestaurants] = useState( resList);
+
   return (
     <div className="body">
       <div className="filter">
@@ -38,7 +13,8 @@ const Body = () => {
             (res)=>res.info.avgRating>4
           )
           setListOfRestaurants(filteredList);
-        }}>Top Rated Restaurants</button>
+        }}
+        >Top Rated Restaurants</button>
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant) => (
